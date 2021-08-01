fb435324a06e445598b75718446d31756cc829e5
TechnoJo4 <technojo4@gmail.com>
2021-07-31 23:05:55 -0400

    Move factorize to Primes

--- Primes.hs
+++ Primes.hs
@@ -5,3 +5,9 @@ primes = 2 : 3 : 5 : (drop 1 $ filter (\n -> not
         $ any (\x -> n `mod` x == 0)
         $ takeWhile (<= (ceiling $ sqrt $ fromIntegral n)) primes)
         [4..])
+
+factorize :: Int -> [Int]
+factorize n = let maxfactor = ceiling $ sqrt $ fromIntegral n in
+        foldl factorize' [n] $ takeWhile (<= maxfactor) primes
+        where factorize' l@(x:xs) prime = let (divp, modp) = x `divMod` prime in
+                if modp == 0 then factorize' (divp:prime:xs) prime else l
